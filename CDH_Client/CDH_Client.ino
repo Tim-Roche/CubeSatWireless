5b7dd484c69ec907b2525b7d79a3fb60ebbde381
Tim Roche Laptop <kc2vsa@gmail.com>
2019-10-21 22:06:25 -0400

    Got autosubscribe to notifications workinggit add .!

--- CDH_Client/CDH_Client.ino
+++ CDH_Client/CDH_Client.ino
@@ -23,6 +23,11 @@ static unsigned long lastEvent = 0;
 static unsigned long threshold = 10000;
 boolean scanning = true;
 
+
+//Characteristic Map
+byte REGNOTIF = 0x01;
+std::map<std::string,byte> charMap; //Current handles notification registration
+
 boolean newMail = false;
 
 BLERemoteCharacteristic* newRemoteChar;
@@ -71,18 +76,22 @@ bool connectToServer() {
     Serial.println("Created client");
     pClient->setClientCallbacks(new MyClientCallback());
     pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
-    autoDiscover(pClient);
+    autoDiscover(pClient, true);
     Serial.println(" - Connected to server");
     
     Serial.println("Initialization Complete!");
     return(true);
 }
 
-void autoDiscover(BLEClient* pClient)
+void autoDiscover(BLEClient* pClient, bool subscribe)
 {
   Serial.println("Avaliable Services:");
+  std::map<std::string, byte>::iterator subMapItr; //Iterator for hashmap for auto register
+  
   std::map<std::string, BLERemoteService*>* services = pClient->getServices();
   std::map<std::string, BLERemoteService*>::iterator itr;
+  
+  std::string selected_char; //Going to be the string of the currently select charecteristic
   for (itr = services->begin(); itr != services->end(); ++itr)
   {
     Serial.println((itr->first).c_str());
@@ -90,12 +99,24 @@ void autoDiscover(BLEClient* pClient)
     std::map<std::string, BLERemoteCharacteristic*>::iterator charItr;
     for (charItr = character->begin(); charItr != character->end(); ++charItr)
     {
+      selected_char = charItr->first; //Grab the selected charecteristic UUID in string form
       Serial.print("---");
       Serial.println((charItr->first).c_str());
+      
+      //Notification Registration if selected
+      if(subscribe)
+      {
+        subMapItr = charMap.find(selected_char);
+        if ((subMapItr != charMap.end()) && ((subMapItr->second)&REGNOTIF != 0))
+        {
+          BLERemoteCharacteristic* selected_BLERemoteChar = charItr->second;
+          selected_BLERemoteChar->registerForNotify(notifyCallback);
+          Serial.println("--- Found in hashmap, registered for notifications!");
+        }
+      }
     }
   }
-}
-
+}; 
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice advertisedDevice) {
@@ -117,9 +138,16 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   } 
 }; 
 
-void setup() {
+void setup() 
+{  
   Serial.begin(115200);
   Serial.println("Starting Arduino BLE Client application...");
+
+  //User Charecteristic Registration Requirements
+  //Eventually there will be a way for user to enter what they want charecteristics they want to register for
+  //For now its hard coded. Deal with it.
+  charMap.insert(std::pair<std::string,byte>("f9fd0001-71ae-42c4-bd19-9d5e37ebf073",REGNOTIF));
+  
   BLEDevice::init("");
   BLEScan* pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
